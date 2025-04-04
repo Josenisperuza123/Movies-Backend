cedbea8890e1df7fce2ee6d815693a9d84f7c268
Josenisperuza123 <jose.nisperuza@est.iudigital.edu.co>
2025-04-04 14:17:39 -0500

    BD CONF

--- db/connect-mongo.js
+++ db/connect-mongo.js
@@ -1,21 +1,17 @@
 const mongoose = require('mongoose');
 
-
 const getConnection = async () => {
-
     try {
-        const url = "mongodb+srv://nisperuzaarroyojosedomingo:kTotYJgcbNJBMunK@cluster0.qu5v7.mongodb.net/Ing-Web-2?retryWrites=true&w=majority&appName=Cluster0"   
+        const url = process.env.MONGO_URI; // Ahora usa la variable de entorno
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
 
-        await mongoose.connect(url);
-
-        console.log('conexion exitosa');
-
-    } catch(error) {
-        console.log(error);   
+        console.log('✅ Conexión exitosa a MongoDB');
+    } catch (error) {
+        console.error('❌ Error conectando a MongoDB:', error);
     }
-}
-    
-    module.exports = {
-        getConnection,
-    }
+};
 
+module.exports = { getConnection };
