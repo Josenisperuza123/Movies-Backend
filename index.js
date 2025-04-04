b225b2f1f81d46b5e2b6fea08ab987bb8d2f149c
Josenisperuza123 <jose.nisperuza@est.iudigital.edu.co>
2025-04-04 13:42:09 -0500

    agg cors

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-const express = require('express'); 
-const cors = require('cors'); // Importar CORS const connectDB = require('./db/connect-mongo'); // Conexión a MongoDB Atlas require('dotenv').config();
-const app = express(); const port = process.env.PORT || 8000; // Puerto por defecto
-
- app.use(cors());
- app.use(express.json());
-
-connect-mongo.PORT()
-.then(() => console.log('\u2705 Conectado a MongoDB Atlas'))
-.catch(err => console.error('\u274C Error conectando a MongoDB:', err));
-
-app.use('/productora', require('./routes/productora'));
-app.use('/director', require('./routes/director')); 
-app.use('/genero', require('./routes/genero'));
-app.use('/tipo', require('./routes/tipo')); 
-app.use('/media', require('./routes/media'));
-
-
-app.listen(port, () => {
-   console.log('\u{1F680} Servidor escuchando en el puerto ${port}'); 
+const express = require('express')
+const { getConnection } = require('./db/connect-mongo')
+const cors = require('cors');
+require('dotenv').config()
   
-  });
-
+  
+const app = express()
+const port = process.env.PORT;
+  
+app.use(cors());
+  
+getConnection();
+  
+app.use(express.json());
+  
+  app.use('/productora', require('./routes/productora'));
+  app.use('/director', require('./routes/director')); 
+  app.use('/genero', require('./routes/genero'));
+  app.use('/tipo', require('./routes/tipo')); 
+  app.use('/media', require('./routes/media'));
+  
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+  
